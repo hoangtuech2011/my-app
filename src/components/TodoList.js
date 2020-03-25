8b81b2a0c9d34cfd6cb1a276d54a4b3505d7e0cf
hoangtuech2011 <lvdoanhkhtn@gmail.com>
2020-03-25 11:07:08 +0700

    release v.0.0.2

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,6 +6,7 @@ const TodoList = ({ todos, toggleTodo }) => (
   <ul>
     {
       confirmonsole.log('release v.0.0.2');
+      confirmonsole.log('happy coding');
     todos.map(todo => (
       <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
     ))}
