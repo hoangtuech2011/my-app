e13225a23863f1071d8dc9e123d6bb52b5bb4cd5
hoangtuech2011 <lvdoanhkhtn@gmail.com>
2020-03-25 10:24:27 +0700

    add todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Todo from './Todo'
-import CustomTodo from './CustomTodo'
+import React from "react";
+import PropTypes from "prop-types";
+import Todo from "./Todo";
+import CustomTodo from "./CustomTodo";
 
 const TodoList = ({ todos, toggleTodo }) => (
   <ul>
@@ -9,7 +9,7 @@ const TodoList = ({ todos, toggleTodo }) => (
       <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
     ))}
   </ul>
-)
+);
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(
     PropTypes.shape({
@@ -19,26 +19,24 @@ TodoList.propTypes = {
     }).isRequired
   ).isRequired,
   toggleTodo: PropTypes.func.isRequired
-}
-export default TodoList
+};
+export default TodoList;
 
-// const TodoList = ({ customTodos, toggleTodo }) => (
-//   <ul>
-//     {customTodos.map(todo => (
-//       <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
-//     ))}
-//   </ul>
-// )
-// TodoList.propTypes = {
-//   customTodos: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//       completed: PropTypes.bool.isRequired,
-//       text: PropTypes.string.isRequired
-//     }).isRequired
-//   ).isRequired,
-//   toggleTodo: PropTypes.func.isRequired
-// }
-// export default TodoList
-
-
+const TodoList = ({ customTodos, toggleTodo }) => (
+  <ul>
+    {customTodos.map(todo => (
+      <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
+    ))}
+  </ul>
+);
+TodoList.propTypes = {
+  customTodos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  toggleTodo: PropTypes.func.isRequired
+};
+export default TodoList;
